060f69b0f23bfaa8500f130840808dcc61ea96f6
Jeremy <liruijie0807@163.com>
2024-03-29 14:23:02 +0800

    feat: finished readData.py

--- TextSearch/readData.py
+++ TextSearch/readData.py
@@ -11,8 +11,3 @@ print(len(data))
 with open("D:/Download/dqs_address.json", "w", encoding="utf-8") as file:
     json.dump(data, file, ensure_ascii=False)
 
-print("转换完成")
-
-# 读取JSON文件
-with open('D:/Download/dqs_address.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
