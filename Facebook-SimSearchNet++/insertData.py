dcac82bec2bd1ed2858acc82309a601e5c35b93d
Jeremy <liruijie0807@163.com>
2024-04-22 19:49:37 +0800

    feat: add Facebook-SimSearchNet/insertData.py

--- Facebook-SimSearchNet++/insertData.py
+++ Facebook-SimSearchNet++/insertData.py
@@ -1,5 +1,6 @@
 import json
 
+import numpy as np
 from pymilvus import (
     connections,
     FieldSchema,
@@ -10,6 +11,24 @@ from pymilvus import (
 )
 
 import time
+
+def read_fbin(filename, start_idx=0, chunk_size=None):
+    """ Read *.fbin file that contains float32 vectors
+    Args:
+        :param filename (str): path to *.fbin file
+        :param start_idx (int): start reading vectors from this index
+        :param chunk_size (int): number of vectors to read.
+                                 If None, read all vectors
+    Returns:
+        Array of float32 vectors (numpy.ndarray)
+    """
+    with open(filename, "rb") as f:
+        nvecs, dim = np.fromfile(f, count=2, dtype=np.int32)
+        # print(nvecs, dim)
+        nvecs = (nvecs - start_idx) if chunk_size is None else chunk_size
+        arr = np.fromfile(f, count=nvecs * dim, dtype=np.uint8,
+                          offset=start_idx * 4 * dim)
+    return arr.reshape(nvecs, dim)
 
 # client = MilvusClient(
 #     uri="http://10.100.2.241:19530"
@@ -22,39 +41,43 @@ client = MilvusClient(
 )
 # 指定每个字段的 Schema
 entry_id = FieldSchema(name="entry_id", dtype=DataType.INT64, is_primary=True)
-entry_text = FieldSchema(name="entry_text", dtype=DataType.VARCHAR, max_length=1024)
-entry_vector = FieldSchema(name="entry_vector", dtype=DataType.FLOAT_VECTOR, dim=384)
+entry_vector = FieldSchema(name="entry_vector", dtype=DataType.FLOAT_VECTOR, dim=256)
 
 # 指定集合的 Schema
-schema = CollectionSchema(fields=[entry_id, entry_text, entry_vector])
+schema = CollectionSchema(fields=[entry_id, entry_vector])
 
 # 创建空集合
-client.drop_collection(collection_name="TextData")
-client.create_collection(collection_name="TextData", schema=schema)
+client.drop_collection(collection_name="Facebook_SimSearchNet")
+client.create_collection(collection_name="Facebook_SimSearchNet", schema=schema)
 
-with open("/home/bangsun-f/Jeremy/dataset/TextSearch/dqs_address.json", "r", encoding="utf-8") as file:
-    textData = json.load(file)
+# data = read_fbin("FB_ssnpp_database.u8bin")
 
-with open("/home/bangsun-f/Jeremy/dataset/TextSearch/dqs_address_embeddings.json", "r", encoding="utf-8") as file:
-    textVec = json.load(file)
-
-num = len(textData)
-data_id = [i for i in range(num)]
+num = 1000000000
+# data_id = [i for i in range(num)]
 tic = time.perf_counter()
 i, step = 0, 2100
 
 while i < num:
     # 将数据插入集合
-    cur_data = []
+    cnt = min(num - i, step)
+    cur_data = read_fbin("/home/bangsun-f/Jeremy/dataset/Facebook-SimSearchNet++/FB_ssnpp_database.u8bin", i, cnt)
     ptr = i
+    new_data = []
+    for d in cur_data:
+        data_list = []
+        for dd in d:
+            data_list.append(float(dd))
+        new_data.append(data_list)
+    cur_data = new_data
+    insert_data = []
     while (ptr < num) and ptr < i + step:
-        cur_data.append({"entry_id": data_id[ptr], "entry_text": textData[ptr], "entry_vector": textVec[ptr]})
+        insert_data.append({"entry_id": ptr, "entry_vector": cur_data[ptr - i]})
         ptr += 1
     client.insert(
-        collection_name="TextData",
-        data=cur_data
+        collection_name="Facebook_SimSearchNet",
+        data=insert_data
     )
-    i += step
+    i = ptr
     print(i)
 toc = time.perf_counter()
 print(f"inserted {num} entities in {toc - tic:0.4f} seconds")
