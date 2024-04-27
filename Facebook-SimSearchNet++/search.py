3f1bf271f7a433ce56e774485876525a3aac1b40
Jeremy <liruijie0807@163.com>
2024-04-27 14:13:24 +0800

    feat: modify search.py

--- Facebook-SimSearchNet++/search.py
+++ Facebook-SimSearchNet++/search.py
@@ -1,4 +1,5 @@
 import json
+import random
 import time
 
 from pymilvus import MilvusClient, connections, Collection
@@ -9,32 +10,30 @@ client = MilvusClient(
     db_name="default"
 )
 
-# load text and embeddings
-with open("/home/bangsun-f/Jeremy/dataset/TextSearch/dqs_address.json", "r", encoding="utf-8") as file:
-    textData = json.load(file)
-
-with open("/home/bangsun-f/Jeremy/dataset/TextSearch/dqs_address_embeddings.json", "r", encoding="utf-8") as file:
-    textVec = json.load(file)
 client.load_collection(collection_name="TextData")
 num = 10
+data = []
+for i in range(num):
+    data.append([random.random() * 2 - 1 for _ in range(256)])
+
 tic = time.perf_counter()
 
 for i in range(num):
-    print("Query textData is ", textData[i])
+    print("Query textData is ", data[i])
 
     res = client.search(
-        collection_name="TextData",  # Replace with the actual name of your collection
+        collection_name="Facebook_SimSearchNet",  # Replace with the actual name of your collection
         # Replace with your query entry_idctorentry_text
-        data=[textVec[i]],
+        data=[data[i]],
         anns_field="entry_vector",
         limit=5,  # Max. number of search results to return
         search_params={"metric_type": "L2", "params": {"nprobe": 10}},  # Search parameters
-        output_fields=["entry_id", "entry_text"],
+        output_fields=["entry_id", "entry_vector"],
     )
 
     result = json.dumps(res, indent=4)
     with open("output.json", "a", encoding="utf-8") as file:
-        file.write("Query data is " + result + "\n")
+        file.write("Query data is " + data[i] + "\n")
         file.write(result)
 
 toc = time.perf_counter()
